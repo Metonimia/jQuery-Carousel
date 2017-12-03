d2c13e774c54cc7f978518206148b5351ea510c3
Zuzanna <zuzanna.zuzewicz@gmail.com>
2017-12-03 18:40:08 +0200

    Added project structure

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+console.log("hi!");
+
+$(function(){
+	 
+	var carouselList = setInterval(function changeSlide() {
+
+		$("#carousel ul").animate({'marginLeft':-400}, 500, function moveFirstSlide() {
+				var firstItem = $(this).find("li:first");
+				var lastItem = $(this).find("li:last");
+				lastItem.after(firstItem);
+				$(this).css({marginLeft:0});
+		})
+
+	}, 3000);
+
+});
+
+
+
