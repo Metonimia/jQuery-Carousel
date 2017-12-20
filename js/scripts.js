5e88242c77d99b8c657f621a9e87af242e11e42d
Zuzia Zuzewicz <zuzannazuzewicz@MacBook-Air-Zuzia.local>
2017-12-20 18:22:12 +0100

    Fixed minor mistakes

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ $(function(){
 	setInterval(function() {
 
 		carouselList.animate({'marginLeft':-400}, 500, function() {
-		var firstItem = $(this).find("li:first");
-		var lastItem = $(this).find("li:last");
-		lastItem.after(firstItem);
-		$(this).css({marginLeft:0});
-		})
+			var firstItem = $(this).find("li:first");
+			var lastItem = $(this).find("li:last");
+			lastItem.after(firstItem);
+			$(this).css({marginLeft:0});
+		});
 
 	}, 3000);
 
